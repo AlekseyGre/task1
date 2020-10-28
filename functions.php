448402c3609d9fe78df2317497c8c8ea29e2bf70
AlekseyGre <sandboxlist@gmail.com>
2020-10-28 19:00:57 +0200

    Очередное изменение проекта

--- functions.php
+++ functions.php
@@ -1,3 +1,4 @@
+
 <?php
 
 
@@ -8,9 +9,10 @@ function huy()
     ?>
     <h1>MAMAWAPADA9</h1>
     <p><?php echo 'DADADA' ?></p>
-    <h1><?php echo 'Привет мир' ?></h1>
     <?php
     var_dump($_SERVER['REQUEST_URI']);
     ?>
     <?php
 }
+
+
---
+++ test-shablon.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Template Name: TEST shablona
+Template Post Type: post, page, product
+*/
+?>
+<?php
+
+get_header();
+?>
+
+<p><?php echo 'Yaaaahoo'?></p>;
+
+
+
+
+
